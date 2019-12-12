6932494bd1dd94eea071faae4cdb12c08401189c
Sarbjeet Singh <ssingh4@corp.hometrust.ca>
2019-12-12 13:25:58 -0500

    day 12

--- answer12.js
+++ answer12.js
@@ -1,198 +1,79 @@
-console.log("answer12");
-var xset = new Set([]);
-var yset = new Set([]);
-var zset = new Set([]);
-var pxset = new Set([]);
-var pyset = new Set([]);
-var pzset = new Set([]);
-// {x:13, y:-13, z:-2},
-// {x:16, y:2, z:-15},
-// {x:7, y:-18, z:-12},
-// {x:-3, y:-8, z:-8},
+console.log("Day 12");
+const foundSetArray = [new Set([]), new Set([]), new Set([])];
+const dimensionSet = [0, 1, 2];
+let satPositions = [
+    [13, -13, -2],
+    [16, 2, -15],
+    [7, -18, -12],
+    [-3, -8, -8]
+];
+satPositions = satPositions.map(el => el.concat([0, 0, 0]));
+let input1 = satPositions.slice(0);
+console.log(satPositions);
 
-// {x:-1, y:0, z:2, px: 0, py: 0, pz: 0},
-// {x:2, y:-10, z:-7, px: 0, py: 0, pz: 0},
-// {x:4, y:-8, z:8, px: 0, py: 0, pz: 0},
-// {x:3, y:5, z:-1, px: 0, py: 0, pz: 0}
-var satPositions = [{sat:1, x:13, y:-13, z:-2, px: 0, py: 0, pz: 0},
-    {sat:2, x:16, y:2, z:-15, px: 0, py: 0, pz: 0},
-    {sat:3, x:7, y:-18, z:-12, px: 0, py: 0, pz: 0},
-    {sat:4, x:-3, y:-8, z:-8, px: 0, py: 0, pz: 0}];
-
+function step(arrInput) {
+    
+    dimensionSet.forEach(dim => {
+        arrInput = calculateVelocity(arrInput, dim);
+    });
+    dimensionSet.forEach(dim => {
+        arrInput = calculateNextStep(arrInput, dim);
+    });
+    return arrInput;
+}
 
 function calculateVelocity(arrInput, dimension) {
-
-    // var sortedArray = arrInput.sort((a,b) => {
-    //     return b[dimension] - a[dimension];
-
-    // });
-
-   return arrInput.map((el, _, arr) => {
-       var smallValues = arr.filter((fel) => fel[dimension] < el[dimension]).length;
-       var biggervalues = arr.filter((fel) => fel[dimension] > el[dimension]).length;
-       el["p"+ dimension] =  el["p"+ dimension] + biggervalues - smallValues;
-    
-       return el;
-   })
+    return arrInput.map((el, _, arr) => {
+        const smallValues = arr.filter((fel) => fel[dimension] < el[dimension]).length;
+        const biggervalues = arr.filter((fel) => fel[dimension] > el[dimension]).length;
+        el[3 + dimension] = el[3 + dimension] + biggervalues - smallValues;
+        return el;
+    })
 }
 
 function calculateNextStep(arrInput, dimension) {
- 
-    return arrInput.map((el) => {
-        el[dimension] = el[dimension] +  el["p"+ dimension] ;
-       
-          
-        
-        return el;
-    });
-    
- 
-
+    return arrInput
+        .map((el) => {
+            el[dimension] = el[dimension] + el[3 + dimension];
+            return el;
+        });
 }
 
-function generateStringCode(arrInput) {
-   var stringCode = '';
-   arrInput.forEach(el => {
-        stringCode = stringCode+"-"+el.sat+"-"+el.x+"-"+el.y+"-"+el.z+"-"+el.px+"-"+el.py+"-"+el.pz;
-   })
-   return stringCode;
+function buildString(array, axis) {
+    let result = "";
+    array.forEach((el) => {
+        result = result + el[axis].toString() + el[axis + 3].toString();
+    });
+    return result;
+}
+const found = [0, 0, 0];
+for (let ij = 0; ij < 1000; ij++) {
+    input1 = step(input1);
+}
+const totalen = input1.map(el => {
+    return el.slice(0,3).reduce((a,b) => { return Math.abs(a) + Math.abs(b) }) * el.slice(3,6).reduce((a,b) => { return Math.abs(a) + Math.abs(b) }); 
+}).reduce((a,b) => {return a + b;})
 
+console.log("Answer to First Part --> " + totalen);
 
-}      
+/// second part 
 
-var strinArray = [];
-console.log(satPositions);
-
-var stringForInput0 = generateStringCode(satPositions);
-var flagx, flagy, flagz;
-var flagpx, flagpy, flagpz;
-for(var i = 0;  ; i ++){
-    if(i % 100000 === 0) {
-   
- console.log(i);
+for (let i = 0;; i++) {
+    dimensionSet.forEach(indx => {
+        const strPos = buildString(satPositions, indx);
+        if (foundSetArray[indx].has(strPos) && !found[indx]) {
+            found[indx] = i;
+        }
+        if (!found[indx]) {
+            foundSetArray[indx].add(strPos);
+        }
+    });
+    if (!found.filter(el => el <= 0).length) {
+        break;
     }
-
-
-var pxpos = satPositions[0].px +"-"+satPositions[1].px+"-"+satPositions[2].px+"-"+satPositions[3].px;
-var pypos = satPositions[0].py +"-"+satPositions[1].py+"-"+satPositions[2].py+"-"+satPositions[3].py;
-var pzpos = satPositions[0].pz +"-"+satPositions[1].pz+"-"+satPositions[2].pz+"-"+satPositions[3].pz;
-var xpos = satPositions[0].x +"-"+satPositions[1].x+"-"+satPositions[2].x+"-"+satPositions[3].x + "-" + pxpos;
-var ypos = satPositions[0].y +"-"+satPositions[1].y+"-"+satPositions[2].y+"-"+satPositions[3].y + "-" + pypos;
-var zpos = satPositions[0].z +"-"+satPositions[1].z+"-"+satPositions[2].z+"-"+satPositions[3].z + "-" + pzpos;
-
-if(xset.has(xpos) && !flagx ){
-    
-    console.log("x-" + i);
-    flagx = i;
-    console.log(satPositions);
-}
-if(yset.has(ypos) && !flagy ){
-    
-    console.log("y-" + i);
-    flagy= i;
-    console.log(satPositions);
-}
-if(zset.has(zpos) && !flagz ){
-    
-    console.log("z-" + i);
-    flagz = i;
-    console.log(satPositions);
+    satPositions = step(satPositions);
 }
 
-if(pxset.has(pxpos)&& !flagpx  ){
-    
-    console.log("px-" + i);
-    flagpx = i;
-    console.log(satPositions);
-}
-if(pyset.has(pypos) && !flagpy){
-    
-    console.log("py-" + i);
-    flagpy = i;
-    console.log(satPositions);
-}
-if(pzset.has(pzpos) && !flagpz ){
-    
-    console.log("pz-" + i);
-    flagpz = i;
-    console.log(satPositions);
-}
-
-if(flagx && flagy && flagz) 
-    {
-    break;
-}
-
-
-
-if(!flagx){
-    xset.add(xpos);
-}
-if(!flagy){
-yset.add(ypos);
-}
-if(!flagz){
-zset.add(zpos);
-}
-if(!flagpx){
-    pxset.add(pxpos);
-}
-if(!flagpy){
-pyset.add(pypos);
-}
-if(!flagpz){
-pzset.add(pzpos);
-}
-
-
-var stringForInput = generateStringCode(satPositions);
-
-
-
-if(stringForInput === stringForInput0 && i !== 0){
-    console.log(i);
-    break;
-}
-
-["x","y","z"].forEach(dim => {
-  
-satPositions = calculateVelocity(satPositions, dim);
-
-
-
-});
-
-["x","y","z"].forEach(dim => {
-  
-    satPositions = calculateNextStep(satPositions, dim);
-   
-    
-    
-    });
-
-
-
-
-}
-
-
-var kineArray = satPositions.map(el => {
-    return ((Math.abs(el.x) + Math.abs(el.y) + Math.abs(el.z))  * (Math.abs(el.px) + Math.abs(el.py) + Math.abs(el.pz)));
-})
-
-
-
-var potArray = satPositions.map(el => {
-    return Math.abs(el.px) + Math.abs(el.py) + Math.abs(el.pz);
-})
-var totalen = 0
-kineArray.forEach(kel => {
-    totalen = totalen + kel;
-});
-
-
-console.log(satPositions);
-console.log(kineArray);
-console.log(potArray);
-console.log(totalen);
-
+const gcd = (a, b) => a ? gcd(b % a, a) : b;
+const lcm = (a, b) => a * b / gcd(a, b);
+console.log("Answer to the second part --> " + found.reduce(lcm));
